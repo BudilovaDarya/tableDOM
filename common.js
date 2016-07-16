b505c653ce0c30ca24f0dbc009e2fd6cd28ee283
BudilovaDarya <BudilovaDarya@gmail.com>
2016-07-16 14:59:25 +0400

    Рисуем таблицу x на y

--- common.js
+++ common.js
@@ -1,15 +1,17 @@
+var table = document.createElement('table'), x = document.getElementById('valX'), y = document.getElementById('valY');
 function generateTable(){
 	var table = document.createElement('table'), row, cell;
     
-	for (j = 0; j < 10; ++j){
+	for (j = 0; j < y.value; ++j){
 		row = table.insertRow();		
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < x.value; i++) {
     	cell = row.insertCell();
 	
 	}
 	}
-document.body.appendChild(table);
+document.getElementById('whereTable').appendChild(table);
 }
 document.getElementById('ok').onclick = function (){
-generateTable();
+	document.getElementById('whereTable').innerHTML=('');	
+	generateTable();
 }
