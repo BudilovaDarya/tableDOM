12028becec0306b9832d70548a10358a533316e9
BudilovaDarya <BudilovaDarya@gmail.com>
2016-07-16 15:37:41 +0400

    Добавлена проверка значений для перерисовывания

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
-var table = document.createElement('table'), x = document.getElementById('valX'), y = document.getElementById('valY');
+var lastX, lastY, table = document.createElement('table'), x = document.getElementById('valX'), y = document.getElementById('valY');
 function generateTable(){
 	var table = document.createElement('table'), row, cell;
     
 	for (j = 0; j < y.value; ++j){
 		row = table.insertRow();		
-	for (i = 0; i < x.value; i++) {
+	for (i = 0; i < x.value; ++i) {
     	cell = row.insertCell();
 	
 	}
@@ -12,6 +12,10 @@ function generateTable(){
 document.getElementById('whereTable').appendChild(table);
 }
 document.getElementById('ok').onclick = function (){
-	document.getElementById('whereTable').innerHTML=('');	
-	generateTable();
+	if (lastX != x.value || lastY != y.value){
+		document.getElementById('whereTable').innerHTML = ('');	
+		generateTable();}
+	lastX = x.value;
+	lastY = y.value;
+	
 }
