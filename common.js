23d83f5b0be2302cd13d7612f70a2fb914176167
BudilovaDarya <BudilovaDarya@gmail.com>
2016-07-17 16:57:12 +0400

    Выводит alert, рисует 10x10 без значений
    Да, я люблю if

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
 var lastX, lastY, table = document.createElement('table'), x = document.getElementById('valX'), y = document.getElementById('valY');
-function generateTable(){
+function generateTable(valx, valy){
 	var table = document.createElement('table'), row, cell;
     
-	for (var j = 0; j <= y.value; ++j){
+	for (var j = 0; j <= valy; ++j){
 		row = table.insertRow();		
-	for (var i = 0; i <= x.value; ++i) {
+	for (var i = 0; i <= valx; ++i) {
     	cell = row.insertCell();
 	if (i != 0 || j != 0){
 		cell.innerText=i*j;
@@ -22,7 +22,8 @@ function generateTable(){
 	}
 	}
 	table.rows[0].cells[0].style.backgroundColor='cyan';
-	for (var i = 1; i < table.rows.length; i++) {
+	if (valx === valy){
+	for (var i = 1; i < table.rows.length; ++i) {
   		table.rows[i].cells[i].style.backgroundColor = 'pink';
   		table.rows[i].cells[i].style.color = 'red';
 	}
@@ -32,13 +33,22 @@ document.getElementById('whereTable').appendChild(table);
 
 
 document.getElementById('ok').onclick = function (){
-	if (lastX != x.value || lastY != y.value){
+	if ((lastX != x.value || lastY != y.value) && (x.value <= 25 && y.value <= 25) && (x.value > 0 && y.value > 0)){
 		document.getElementById('whereTable').innerHTML = ('');	
-		generateTable();}
+		generateTable(x.value, y.value);}
+	if(x.value > 25  || y.value > 25){
+		alert('Попробуй ввести значение меньше 25 ;)');
+	}
+	if (x.value < 0 || y.value <0) {
+		alert('Попробуй ввести значение больше 0');
+	}
+				document.getElementById('whereTable').innerHTML = ('');	
+				generateTable(10,10);
 	lastX = x.value;
 	lastY = y.value;
 	
 }
+
 
 
 	
--- common_innerHTML.js
+++ common_innerHTML.js
@@ -1,11 +1,11 @@
 var lastX, lastY, x = document.getElementById('valX'), y = document.getElementById('valY');
-function generateTable(){
+function generateTable(valx, valy){
 	var row, cell;
-    document.getElementById('whereTable').innerHTML = '<table id="table" width="400" border="1"><tbody></tbody></table>'; 
-	 for (var j = 0; j <= y.value; ++j){
+    document.getElementById('whereTable').innerHTML = '<table id="table"><tbody></tbody></table>'; 
+	 for (var j = 0; j <= valy; ++j){
 		document.getElementsByTagName('tbody')[0].innerHTML += '<tr></tr>'	;
 		row = document.getElementsByTagName('tr')[j];
-	 for (var i = 0; i <= x.value; ++i) {
+	 for (var i = 0; i <= valx; ++i) {
     	row.innerHTML += '<td style="background-color:cyan"></td>'
     	cell = document.getElementsByTagName('tr')[j].getElementsByTagName('td')[i];
     	if (i != 0 || j != 0) {
@@ -26,8 +26,8 @@ function generateTable(){
     	
 		}
 	}
-	if (x.value === y.value){
-	for (var i = 1; i <= y.value; ++i) {
+	if (valx === valy){
+	for (var i = 1; i <= valy; ++i) {
 		cell = document.getElementsByTagName('tr')[i].getElementsByTagName('td')[i];
 		cell.style.backgroundColor = 'pink';
   		cell.style.color = 'red';
@@ -38,9 +38,19 @@ function generateTable(){
 
 
 document.getElementById('ok').onclick = function (){
-	if (lastX != x.value || lastY != y.value){
+	if ((lastX != x.value || lastY != y.value) && (x.value <= 25 && y.value <= 25) && (x.value > 0 && y.value > 0)){
 		document.getElementById('whereTable').innerHTML = ('');	
-		generateTable();}
+		generateTable(x.value, y.value);}
+	if(x.value > 25  || y.value > 25){
+		alert('Попробуй ввести значение меньше 25 ;)');
+	}
+	if (x.value < 0 || y.value <0) {
+		alert('Попробуй ввести значение больше 0');
+	}
+	else 		{
+				document.getElementById('whereTable').innerHTML = ('');	
+				generateTable(10,10);
+			}
 	lastX = x.value;
 	lastY = y.value;
 	
